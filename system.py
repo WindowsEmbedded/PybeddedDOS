45df3ef081baa49918944b547e385031605e7e24
WindowsEmbedded <embedded09@outlook.com>
2023-01-19 17:50:45 +0800

    删除目录

--- system.py
+++ system.py
@@ -11,6 +11,7 @@
 import sys
 #from exeux import *
 import json
+import getpass
 import socket
 VER = "0.2.7-beta1"
 USERNAME = "root"
@@ -24,6 +25,56 @@ dirs = {
 		"bin/":{}
 	}
 }
+
+def Encrypt(text,key):
+	LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ" #大写字母表
+	letters=LETTERS.lower()              #小写字母表
+	EncryptedText = ""                   #加密后的文本
+	for char in text: #遍历text
+		if char in LETTERS: #是大写
+			num = LETTERS.find(char)
+			num += key
+			if num >= len(LETTERS): #大于字母表长度
+				num -= len(LETTERS)
+			elif num < 0:
+				num += len(LETTERS)
+			EncryptedText += LETTERS[num]
+		elif char in letters: #是小写
+			num = letters.find(char)
+			num += key
+			if num >= len(letters): #大于字母表长度
+				num -= len(letters)
+			elif num < 0:
+				num += len(letters)
+			EncryptedText += letters[num]
+		else:
+			EncryptedText += char #拼接字母以外的字符
+	return EncryptedText
+
+def Decrypt(password,key):
+	LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ" #大写字母表
+	letters=LETTERS.lower()              #小写字母表
+	DecryptedText = ""                   #加密后的文本
+	for char in password:
+		if char in LETTERS: #是大写
+			num = LETTERS.find(char)
+			num -= key
+			if num >= len(LETTERS): #大于字母表长度
+				num -= len(LETTERS)
+			elif num < 0:
+				num += len(LETTERS)
+			DecryptedText += LETTERS[num]
+		elif char in letters: #是小写
+			num = letters.find(char)
+			num -= key
+			if num >= len(letters): #大于字母表长度
+				num -= len(letters)
+			elif num < 0:
+				num += len(letters)
+			DecryptedText += letters[num]
+		else:
+			DecryptedText += char #拼接字母以外的字符
+	return DecryptedText
 
 def GetDirectoryContents(dirname=str()) -> dict: #获取某目录的文件
 	global dirs
@@ -46,7 +97,16 @@ def newdir(pathname=str(),dirname=str()):
 		f.close()
 	#print(dirs)
 
-
+def deldir(pathname=str(),dirname=str()): #原理和上面的差不多
+	global dirs 
+	dires = GetDirectoryContents(pathname)
+	if not "%s/"%dirname in dires: #判断要删除的文件夹是否存在
+		print("ERROR: %s isn't in %s"%(dirname,pathname))
+	else:
+		dires.pop("%s/"%dirname) #删除某字典
+		with open("path.json",'w') as f: 
+			f.write(json.dumps(dirs))
+			f.close()
 
 def main():
 	global VER
@@ -78,8 +138,13 @@ def main():
 			elif(command[0] == "mkdir" or
 				 command[0] == "md" #新建文件夹
 			):
-				if command[3] == "--absolute-path": newdir(command[2],command[1])
+				#if command[3] == "--absolute-path": newdir(command[2],command[1])
 				newdir(PWD,command[1])
+			elif(command[0] == "rmdir" or
+				 command[0] == "rd" #删除文件夹
+			):
+				#if command[3] == "--absoulute-path": deldir(command[2],command[1])
+				deldir(PWD,command[1])
 			else:
 				print("Unknown command")
 		except IndexError:
@@ -93,10 +158,13 @@ def init():
 		with open("path.json") as f:
 			dirs = json.load(f)
 			usrname = input("Username: ")
-			if "%s/"%usrname in dirs['/']['usr/']:
+			if "%s/"%usrname in dirs['/']['usr/'] and "password" in dirs['/']['usr/']['%s/'%usrname]:
+				#pswd = getpass.getpass("Password: ")
+				#if pswd == Decrypt(dirs['/']['usr/']['%s/'%usrname],26):
 				USERNAME = usrname
 				PWD = "/usr/%s/home/"%USERNAME
 				return
+				#else: print("Wrong password")
 			else: print("%s's path is not found"%usrname)
 			sys.exit(0xff)
 			#except SystemExit: pass
